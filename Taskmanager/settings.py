0936d81b5d62fa737b38f135f4c482cb88749ab0
AlexeiShirobokov <alexeimvc@gmail.com>
2025-09-03 18:07:10 +1000

    Продакшн

--- Taskmanager/settings.py
+++ Taskmanager/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = "django-insecure-%j+^#ukmtl#e!a91pb+!qe6#3y&e+9t%6o5ugki+whe_-t$lp3"
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+#DEBUG = True
 
 ALLOWED_HOSTS = ['*']
 
@@ -120,7 +120,8 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/5.2/howto/static-files/
 
 STATIC_URL = "static/"
-STATICFILES_DIRS = [BASE_DIR / 'static']
+STATIC_ROOT = BASE_DIR / 'staticfiles'
+#STATICFILES_DIRS = [BASE_DIR / 'static']
 
 MEDIA_URL = '/media/'
 MEDIA_ROOT = BASE_DIR / 'media'
